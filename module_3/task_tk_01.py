23d902a3416cc73c2c92ea8459f0b71ff914ebe2
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2023-12-18 22:53:57 +0200

    update

--- module_3/task_tk_01.py
+++ module_3/task_tk_01.py
@@ -1,8 +1,19 @@
 import tk_coord
+import numpy as np
+
+A = np.array([[2, 2],
+              [3, 4]])
+
+B = np.array([[5, 6],
+              [7, 8]])
+
+C = A.dot(B)
 
 
 #  задамо один одиничний вектор
 if __name__ == "__main__":
 
-    tk_coord.vector(0, 0, 1, 1)
-    tk_coord.root.mainloop()
+    # tk_coord.vector(0, 0, 1, 1)
+    # tk_coord.root.mainloop()
+
+    print(C)
